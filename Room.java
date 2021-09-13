2039f6c96448481e46bd1e522f6f10df6c51fda4
Kevin Lopez <lopez.kevina5@gmail.com>
2021-09-13 10:17:59 -0400

    Update Room.java
    Added code for Room class

--- Room.java
+++ Room.java
@@ -1,3 +1,130 @@
 public class Room {
+    //Variables needed for room class 
+    private String roomName;
+    private double roomLength;
+    private double roomWidth;
+    private String shadeAmount;
+    private double btuNeeded;
+
+    /**
+     * Construcor with no args
+     */
+    public Room(){ 
+        roomName = "";
+        roomLength = 0.0;
+        roomWidth = 0.0;
+        shadeAmount = "";
+        btuNeeded = 0.0;
+    }
+
+    /**
+     * Constructor 
+     * @param rName the room name 
+     * @param rLength room length 
+     * @param rWidth room width 
+     * @param sAmount shade amount of the room 
+     * @param btu BTUs needed to heat room
+     */
+    public Room(String rName, double rLength, double rWidth, String sAmount, double btu){
+        roomName = rName;
+        roomLength = rLength;
+        roomWidth = rWidth;
+        shadeAmount = sAmount;
+        btuNeeded = btu;
+
+    }
+
+    /**
+     * Setters
+     * mutators to set the value of the fields of the class
+     */
+
+     public void setName(String rName){
+         roomName = rName;
+
+     }
+
+     public void setLength(double rLength){
+         roomLength = rLength;
+     }
+
+     public void setWidth(double rWidth){
+         roomWidth = rWidth;
+     }
+
+     public void setShade(String sAmount){
+         shadeAmount = sAmount;
+     }
+
+     public void setBTU(double btu){
+         btuNeeded = btu;
+     }
+
+     /**
+      * Getters
+      a acccesor to get the value of fields in the class
+      */
+
+      public String getName(){
+          return roomName;
+      }
+
+      public double getLength(){
+          return roomLength;
+      }
+
+      public double getWidth(){
+          return roomWidth;
+      }
+
+      public String getShade(){
+          return shadeAmount;
+      }
+
+      public double getBTU(){
+          return btuNeeded;
+      }
+
+      /**
+       * Area method 
+       * No arg method that returns area 
+       */
+      public double getArea(){
+          return roomWidth * roomLength;
+      }
+
+      /**
+       * BTU calculation method 
+       * No arg method that retunrns BTUs needed for room 
+       * If Little shade, BTU should be increased by 15%
+       * If Abundant shade,  BTU should be decreased by 10%
+       */
+
+       public double getBTUs(){
+           double area = roomLength * roomWidth;
+
+           if(area < 250){
+               btuNeeded = 5500;
+           } else if(area <= 500 ){
+               btuNeeded = 10000;
+           } else if (area < 1000){
+               btuNeeded = 17500;
+           }else {
+               btuNeeded = 24000;
+           }
+
+
+           if(shadeAmount == "Little"){
+               btuNeeded *= 1.15;
+           }
+
+           if(shadeAmount == "Abundant"){
+               btuNeeded *= .90;
+           }
+
+           return btuNeeded;
+
+       }
+
     
 }
